69b9b32dcdbfb719975cc37ef7b0242248cc85ef
Vuong Dang <ddvuong@gmail.com>
2023-11-08 00:28:25 +0100

    Add AssetBundle to serve static assets from the root path

--- dropwizard-kotlin-nextjs/src/main/kotlin/dev/vuongdang/dropwizard/app/MainApplication.kt
+++ dropwizard-kotlin-nextjs/src/main/kotlin/dev/vuongdang/dropwizard/app/MainApplication.kt
@@ -2,6 +2,7 @@ package dev.vuongdang.dropwizard.app
 
 import dev.vuongdang.dropwizard.config.Config
 import dev.vuongdang.dropwizard.resource.HelloWorldResource
+import io.dropwizard.assets.AssetsBundle
 import io.dropwizard.core.Application
 import io.dropwizard.core.setup.Bootstrap
 import io.dropwizard.core.setup.Environment
@@ -10,8 +11,9 @@ class MainApplication : Application<Config>() {
 
     override fun initialize(bootstrap: Bootstrap<Config>?) {
         // Initialization logic here
+        bootstrap!!.addBundle(AssetsBundle("/assets/", "/"))
     }
-    
+
     override fun run(configuration: Config, environment: Environment) {
         // Set up your resources, health checks, etc.
         environment.jersey().register(HelloWorldResource())
