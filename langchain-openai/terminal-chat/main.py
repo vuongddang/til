915537daf1b85b5297166263bf004cb19d014a7b
Vuong Dang <ddvuong@gmail.com>
2023-12-08 22:43:59 +0100

    Uses a ConversationSummaryMemory for terminal chatbot

--- langchain-openai/terminal-chat/main.py
+++ langchain-openai/terminal-chat/main.py
@@ -1,17 +1,17 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import ConversationSummaryMemory, FileChatMessageHistory
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
 
 load_dotenv()
 
-chat = ChatOpenAI()
-memory  = ConversationBufferMemory(
-    # Store chat history in messages.json
-    chat_memory=FileChatMessageHistory("messages.json"),
+chat = ChatOpenAI(verbose=False)
+# With ConversationSummaryMemory, for every call to OpenAI api, there's additional call to sumarize the conversation to be used as input for the next call
+memory  = ConversationSummaryMemory(
     memory_key="messages", 
-    return_messages = True
+    return_messages=True,
+    llm=chat # llm uses to sumarize the conversation
 )
 prompt = ChatPromptTemplate(
     input_variables=["content", "messages"],
@@ -24,7 +24,8 @@ prompt = ChatPromptTemplate(
 chain = LLMChain(
     llm = chat,
     prompt = prompt,
-    memory = memory
+    memory = memory,
+    verbose=True # see what we send to llm
 )
 while True:
     content = input(">> ")
